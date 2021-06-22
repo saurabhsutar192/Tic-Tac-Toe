bdebdf1855a8620e82c3c286c7856a127f2229ec
Saurabh Sutar <saurabhsutar192@gmail.com>
2021-06-22 18:19:26 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,110 @@
+let box = document.querySelectorAll(".box");
+let restart = document.querySelector(".restart");
+
+let switcher = false;
+
+let handleClick = (e) => {
+	switcher = !switcher;
+	console.log(e.target);
+	let clickedBox = e.target;
+	if (switcher) {
+		clickedBox.innerHTML = "X";
+	} else {
+		clickedBox.innerHTML = "O";
+	}
+	clickedBox.removeEventListener("click", handleClick);
+
+	setTimeout(compare, 100);
+};
+
+let compare = () => {
+	let a1 = box[0].innerHTML;
+
+	let a2 = box[1].innerHTML;
+	let a3 = box[2].innerHTML;
+	let b1 = box[3].innerHTML;
+	let b2 = box[4].innerHTML;
+	let b3 = box[5].innerHTML;
+	let c1 = box[6].innerHTML;
+	let c2 = box[7].innerHTML;
+	let c3 = box[8].innerHTML;
+
+	if (a1 === a2 && a1 === a3 && a1 !== "") {
+		if (a1 === "X") {
+			window.alert("X wins");
+		} else {
+			window.alert("O wins");
+		}
+		location.reload();
+	} else if (b1 === b2 && b1 === b3 && b1 !== "") {
+		if (b1 === "X") {
+			window.alert("X wins");
+		} else {
+			window.alert("O wins");
+		}
+		location.reload();
+	} else if (c1 === c2 && c1 === c3 && c1 !== "") {
+		if (c1 === "X") {
+			window.alert("X wins");
+		} else {
+			window.alert("O wins");
+		}
+		location.reload();
+	} else if (a1 === b1 && a1 === c1 && a1 !== "") {
+		if (a1 === "X") {
+			window.alert("X wins");
+		} else {
+			window.alert("O wins");
+		}
+		location.reload();
+	} else if (a2 === b2 && a2 === c2 && a2 !== "") {
+		if (a2 === "X") {
+			window.alert("X wins");
+		} else {
+			window.alert("O wins");
+		}
+		location.reload();
+	} else if (a3 === b3 && a3 === c3 && a3 !== "") {
+		if (a3 === "X") {
+			window.alert("X wins");
+		} else {
+			window.alert("O wins");
+		}
+		location.reload();
+	} else if (a1 === b2 && a1 === c3 && a1 !== "") {
+		if (a1 === "X") {
+			window.alert("X wins");
+		} else {
+			window.alert("O wins");
+		}
+		location.reload();
+	} else if (a3 === b2 && a3 === c1 && a3 !== "") {
+		if (a3 === "X") {
+			window.alert("X wins");
+		} else {
+			window.alert("O wins");
+		}
+		location.reload();
+	} else if (
+		a1 !== "" &&
+		b1 !== "" &&
+		c1 !== "" &&
+		a2 !== "" &&
+		b2 !== "" &&
+		c2 !== "" &&
+		a3 !== "" &&
+		b3 !== "" &&
+		c3 !== ""
+	) {
+		window.alert("tied");
+		location.reload();
+	}
+};
+
+box.forEach((box) => {
+	box.addEventListener("click", handleClick);
+});
+
+restart.addEventListener("click", () => {
+	location.reload();
+});
